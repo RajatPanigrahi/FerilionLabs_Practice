95c489f9c15c76e86d294a92c6a9aaf641cb7fb2
Rajat <rajatpanigrahi96@gmail.com>
2026-03-10 16:42:35 +0530

    new python file is added

---
+++ Day1.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter Second number: "))
+num3 = input("Enter third number: ")
+sum = num1+num2
+print("Sum is: ", sum)
+print("Datatype of num1 is: ", type(num1))
+print("Datatype of num2 is:",type(num2))
+print("Datatype of num3 is:",type(num3))
+
+
+
